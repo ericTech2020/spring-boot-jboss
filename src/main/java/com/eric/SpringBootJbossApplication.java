0d520b83811eb7634666a729348d11a5de65df3e
Eric <nuseric1983@gmail.com>
2020-05-13 16:18:56 +0800

    first commit

---
+++ src/main/java/com/eric/SpringBootJbossApplication.java
@@ -0,0 +1,42 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootJbossApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<SpringBootJbossApplication> applicationClass = SpringBootJbossApplication.class;
+}
+
+
+
+
+
+@RestController
+class HelloController {
+
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name) {
+
+		return "Hi "+name+" !";
+
+	}
+}
+
+
+
